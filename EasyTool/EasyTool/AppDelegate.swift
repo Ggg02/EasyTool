5904b2d4c17cdd091eb2c16f8cc98ceecc7bdbf5
alex <alex@alexde-MacBook-Pro.local>
2017-03-22 13:44:58 +0800

    first time Commit

---
+++ EasyTool/EasyTool/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  EasyTool
+//
+//  Created by alex on 2017/3/22.
+//  Copyright © 2017年 alex. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
